c0cc9889419f287fc5fa143ddb41efc7aa1aaf36
James Caleca <jacaleca2@gmail.com>
2021-01-31 18:19:04 -0600

    DJ JS - made code a little cleaner and did part of assignment I originally missed doing.

--- dom-1/dj-js/app.js
+++ dom-1/dj-js/app.js
@@ -5,6 +5,23 @@ document.getElementById("square").addEventListener("mouseup", mouseUp);
 document.getElementById("square").addEventListener("dblclick", dblClick);
 document.addEventListener("wheel", mouseWheel);
 
+window.addEventListener("keydown", changeColor) 
+
+    function changeColor() {
+        console.log(square.style.background)
+        if (event.which === 82) {
+            square.style.background = "red"
+        } else if (event.which === 89){
+            square.style.background = "yellow"
+        } else if (event.which === 71){
+            square.style.background = "green"
+        } else if (event.which === 79){
+            square.style.background = "orange"
+        } else if (event.which === 66){
+            square.style.background = "blue"
+        }
+    }
+
 
 function mouseOver() {
     document.getElementById("square").style.backgroundColor = "blue";
@@ -28,4 +45,9 @@ function dblClick() {
 
 function mouseWheel() {
     document.getElementById("square").style.backgroundColor = "orange";
+}
+
+function blueKey() {
+    document.getElementById("square").style.backgroundColor = "blue";
+
 }
