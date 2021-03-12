09ed9b80ed082461a46d60e34d71591f4a42f48a
James Caleca <jacaleca2@gmail.com>
2021-03-11 20:34:53 -0600

    Completed Mario Pest Control assignment

---
+++ js2/mario-pest-control/app.js
@@ -0,0 +1,21 @@
+const form = document.enemies
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const goombaCount = form.goombas.value;
+    form.goombas.value = "";
+    const bobombCount = form.bobombs.value;
+    form.bobombs.value = "";
+    const cheepcheepCount = form.cheepcheeps.value;
+    form.cheepcheeps.value = "";
+
+    goombaCost = goombaCount * 5;
+    bobombCost = bobombCount * 7;
+    cheepcheepCost = cheepcheepCount * 11;
+
+    totalCost = goombaCost + bobombCost + cheepcheepCost;
+
+    document.getElementById("totalCostH2").textContent = `Total Cost: ${totalCost} Coins`
+    
+})
