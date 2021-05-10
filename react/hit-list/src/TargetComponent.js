2092218365037c3adc8380dbec8979cbd92cd894
James Caleca <jacaleca2@gmail.com>
2021-05-10 15:18:13 -0500

    Meme generator initial commit

--- react/hit-list/src/TargetComponent.js
+++ react/hit-list/src/TargetComponent.js
@@ -1,13 +1,62 @@
-import React from "react"
+// import React from "react"
 
-function TargetComponent(props) {
+// function TargetComponent(props) {
     
+//     const completedStyle = {
+//         fontStyle: "italic",
+//         color: "red",
+//         textDecoration: "line-through"
+//     }
+
+//     return (
+//         <div>
+//             <li>
+//                 <figure>
+//                     <img 
+//                         alt={props.name} 
+//                         src={props.image} 
+//                         style={{
+//                             height:"250px", 
+//                             width: "100%"
+//                         }}
+//                     />
+                    
+//                     <figcaption style={{
+//                         fontSize: "18px", 
+//                         padding: "20px 10px", 
+//                         textAlign: "center",
+//                         backgroundColor: "cornflowerblue"
+//                     }}>
+//                         <p style={props.completed ? {completedStyle}: null}>{props.name}</p>
+//                         <input 
+//                             name="checkbox"
+//                             type="checkbox"                             
+//                             onChange={() => props.handleSubmit}
+//                             completed={`${props.completed}`}
+//                         />
+//                         <br/>
+//                     </figcaption>
+//                 </figure>
+//             </li>
+//         </div>
+//     )
+// }
+
+// export default TargetComponent
+
+import React from "react"
+function TargetComponent(props) {
     const completedStyle = {
         fontStyle: "italic",
         color: "red",
         textDecoration: "line-through"
     }
-
+    // const handleInputClick = (event) => {
+    //     if(event.target.checked) {
+    //         console.log("Completed")
+    //     }
+    // }
+    console.log(props)
     return (
         <div>
             <li>
@@ -20,7 +69,6 @@ function TargetComponent(props) {
                             width: "100%"
                         }}
                     />
-                    
                     <figcaption style={{
                         fontSize: "18px", 
                         padding: "20px 10px", 
@@ -31,15 +79,15 @@ function TargetComponent(props) {
                         <input 
                             name="checkbox"
                             type="checkbox"                             
-                            onChange={() => props.handleSubmit}
+                            onClick={() => props.handleInputClick(props.id)}
                             completed={`${props.completed}`}
                         />
                         <br/>
                     </figcaption>
                 </figure>
             </li>
+                {props.completed?<p style={{color: "green", textAlign:"center"}}>completed</p>:<p style={{color: "red", textAlign:"center"}}>not completed</p>}
         </div>
     )
 }
-
 export default TargetComponent
--- react/hit-list/src/TargetList.js
+++ react/hit-list/src/TargetList.js
@@ -1,35 +1,142 @@
+// import React, {Component} from "react"
+// import TargetComponent from "./TargetComponent"
+
+
+// class TargetList extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             targets: [],
+//         }
+//         this.handleSubmit = this.handleSubmit.bind(this)
+//         this.handleChange = this.handleChange.bind(this)
+//     }
+//     componentDidMount(){
+//         fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
+//             .then(response => response.json())
+//             .then((response) => {
+//                 this.setState({
+//                     targets: response
+
+//             })
+            
+//         })
+//     }
+
+//     handleChange(event) {
+//         event.preventDefault()
+//         const {name, value, type, checked} = event.target
+//         checked ? this.setState({[completed]: true}) : this.setState({[name]: value})
+        
+//     }
+
+//     handleSubmit(event) {
+//         event.preventDefault()
+//         this.setState(prevState => {
+//             return {
+//                 targets: [
+//                     ...prevState.targets, {...prevState}
+//                 ],
+                
+//                 name: "",
+//                 image: "",
+//                 completed: true
+//             }
+//         })
+//     }
+
+//     render() {
+//         const mappedTargets = this.state.targets.map((target, index) => {
+//             return <TargetComponent key={index} completed={false} handleChange={this.handleChange} {...target} />
+//         })
+//         console.log(this.state)
+//         return (
+//             <div>
+//                 <ul style={{
+//                     listStyle: "none", 
+//                     display: "grid", 
+//                     gridTemplateColumns: "auto auto auto", 
+//                     gridTemplateRows: "auto auto auto",
+//                     padding: "0px"
+//                 }}>
+//                     {mappedTargets}
+//                 </ul>
+
+//                 <h1>New target:</h1>
+//                 <form id="form" name="new-target" onSubmit={this.handleSubmit}>
+//                     <input 
+//                         style={{margin: "10px"}}
+//                         value={mappedTargets.name}
+//                         type="text" 
+//                         name="name" 
+//                         placeholder="Name" 
+//                         onChange={this.handleChange}
+//                     />
+//                     <br/>
+//                     <input 
+//                         style={{margin: "10px"}}
+//                         value={mappedTargets.image}
+//                         type="text" 
+//                         name="image" 
+//                         placeholder="Image" 
+//                         onChange={this.handleChange}
+//                     />
+//                     <br/>
+//                     <input 
+//                         style={{margin: "10px"}}
+//                         id="submitButton"
+//                         type="submit"
+//                         value="Submit"
+//                     />
+//                 </form>
+//             </div>
+            
+//         )
+//     }
+// }
+
+// export default TargetList
+
 import React, {Component} from "react"
 import TargetComponent from "./TargetComponent"
-
-
 class TargetList extends Component {
     constructor() {
         super()
         this.state = {
-            targets: []
+            targets: [],
+            name: "",
+            image:"",
+            completed: false
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        // this.handleInputClick = this.handleInputClick(this)
     }
     componentDidMount(){
         fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
             .then(response => response.json())
             .then((response) => {
                 this.setState({
-                    targets: response
-
+                    targets: response.map(item => item = {...item, id: Math.random() * 80})
             })
-            
         })
     }
-
     handleChange(event) {
         event.preventDefault()
-        const {name, value, type, completed} = event.target
-        type === "checkbox" ? this.setState({[completed]: true}) : this.setState({[name]: value})
-        
+        const {name, value, type, checked} = event.target
+        this.setState({[name]: value})
+        // checked ? this.setState({[completed]: true}) : this.setState({[name]: value})
     }
-
+    //wouldn't let me use this function unlessn it was an arrow function
+    handleInputClick = (id) => {
+        let filteredTarget = this.state.targets.find(target => target.id === id)
+        let indexFinder = this.state.targets.indexOf(filteredTarget)
+        filteredTarget.completed = !filteredTarget.completed
+        let newTargets = this.state.targets
+        newTargets[indexFinder] = filteredTarget
+        this.setState({targets: newTargets})
+        return console.log(id) 
+    }
     handleSubmit(event) {
         event.preventDefault()
         this.setState(prevState => {
@@ -37,18 +144,17 @@ class TargetList extends Component {
                 targets: [
                     ...prevState.targets, {...prevState}
                 ],
-                
                 name: "",
                 image: "",
+                completed: true
             }
         })
     }
-
     render() {
         const mappedTargets = this.state.targets.map((target, index) => {
-            return <TargetComponent key={index} completed={false} handleChange={this.handleChange} {...target} />
+            return <TargetComponent key={index} handleInputClick={this.handleInputClick} handleChange={this.handleChange} {...target} />
         })
-        console.log({mappedTargets})
+        console.log(this.state)
         return (
             <div>
                 <ul style={{
@@ -60,7 +166,6 @@ class TargetList extends Component {
                 }}>
                     {mappedTargets}
                 </ul>
-
                 <h1>New target:</h1>
                 <form id="form" name="new-target" onSubmit={this.handleSubmit}>
                     <input 
@@ -89,9 +194,7 @@ class TargetList extends Component {
                     />
                 </form>
             </div>
-            
         )
     }
 }
-
 export default TargetList
